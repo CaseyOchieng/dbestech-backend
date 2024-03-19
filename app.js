196cc5c3978dbb0d1598a28067932fe756c8330b
casey <caseyochieng46@gmail.com>
2024-03-19 12:28:47 +0300

    added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const number = Math.round(Math.random() * 100);
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
-require('dotenv').config();
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+require('dotenv').config();
 const app = express();
 
-
-app.use(bodyParser)
-
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+app.use(cors());
+app.options("*", cors());
 
 
 

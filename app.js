eb19402d5669226ca75bc73630dae8b653adad39
casey <caseyochieng46@gmail.com>
2024-03-19 14:22:50 +0300

    added mongoose

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const number = Math.round(Math.random() * 100);
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 require('dotenv').config();
 const app = express();
-
+const uri = process.env.MONGODB_CONNECTION_STRING
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 app.use(cors());
@@ -15,7 +16,15 @@ app.options("*", cors());
 
 
 
-app.listen(port, () => {
-    console.log(`Running on Port ${port} at ${number} ms. Happy Testing`)
-    console.info(`Server is running on http://localhost:${port} at ${number} ms`);
+
+mongoose.connect(uri).then(() => {
+    app.listen(port, () => {
+        console.log(` Server running at ${number} ms. Happy Coding! `);
+        console.info(`Server is running on http://localhost:${port} at ${number} ms`);
+    });
+}).catch((err) => {
+    console.log(err);
 });
+
+
+// mongodb+srv://<username>:<password>@cluster0.nktx1pc.mongodb.net/
